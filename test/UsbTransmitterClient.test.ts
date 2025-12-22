acd9a348b2d06d09b18bd488469952265fca1545
Marc Lammers <marc@lammers.dev>
2025-12-22 21:40:00 +0100

    test: skip integration tests if serial device not present

--- test/UsbTransmitterClient.test.ts
+++ test/UsbTransmitterClient.test.ts
@@ -1,31 +1,39 @@
 import { UsbTransmitterClient } from "../src/UsbTransmitterClient"
 import { ControlCommand, EasyCommand, InfoData } from "../src/domain/enums"
+import * as fs from 'fs'
 
 jest.setTimeout(200000)
 
-const client = new UsbTransmitterClient('/dev/ttyUSB0')
+const devPath = '/dev/ttyUSB0'
+const client = new UsbTransmitterClient(devPath)
 
 const aktiveChannelsForTest = [1, 2]
 
-test.skip('checkChannels', async () => {
-  await client.open()
-  const channels = await client.checkChannels()
-  expect(channels).toEqual(aktiveChannelsForTest)
-  await client.close()
-})
+// Skip tests if hardware not present
+const describeHardware = fs.existsSync(devPath) ? describe : describe.skip
 
-test.skip('getInfo', async () => {
-  await client.open()
-  const response = await client.getInfo(1)
-  console.log(response)
-  expect(response.command).toEqual(EasyCommand.EASY_ACK)
-  await client.close()
-})
+describeHardware('Integration Tests (Hardware)', () => {
 
-test.skip('sendControlCommand', async () => {
-  await client.open()
-  const response = await client.sendControlCommand(1, ControlCommand.down)
-  console.log(response)
-  expect(response.status).toEqual(InfoData.INFO_MOVING_DOWN)
-  await client.close()
+  test('checkChannels', async () => {
+    await client.open()
+    const channels = await client.checkChannels()
+    expect(channels).toEqual(aktiveChannelsForTest)
+    await client.close()
+  })
+
+  test('getInfo', async () => {
+    await client.open()
+    const response = await client.getInfo(1)
+    console.log(response)
+    expect(response.command).toEqual(EasyCommand.EASY_ACK)
+    await client.close()
+  })
+
+  test('sendControlCommand', async () => {
+    await client.open()
+    const response = await client.sendControlCommand(1, ControlCommand.down)
+    console.log(response)
+    expect(response.status).toEqual(InfoData.INFO_MOVING_DOWN)
+    await client.close()
+  })
 })
